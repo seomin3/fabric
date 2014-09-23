3b3ebe7c26396e4107f98bfd4156dc8daca7cc0b
KOO JAHYEON <kjahyeon@gmail.com>
2014-09-23 09:59:10 +0900

    repofile update(foreman, puppet)

--- script/fabfile.py
+++ script/fabfile.py
@@ -17,16 +17,18 @@ def set_ntp():
 	set_service('ntpd', 'start')
 
 def set_yum(arch):
-	run("echo 'nameserver 192.168.122.21' > /etc/resolv.conf")
+	run("echo 'nameserver 8.8.8.8' > /etc/resolv.conf")
 	if arch == 'rhel':
 		put('../repo/local-rhel6.repo', '/etc/yum.repos.d/')
 	if arch == 'cent':
 		put('../repo/local-cent6.repo', '/etc/yum.repos.d/')
 		run('mkdir -p /etc/yum.repos.d/old')
-		run('mv -f /etc/yum.repos.d/CentOS-* /etc/yum.repos.d/old/')
-	if not exists('/etc/yum.repos.d/rpmforge.repo'):
-		ins_repo = 'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm'
-		run('[ ! -f /etc/yum.repos.d/mirrors-rpmforge ] && yum -y -q install '+ ins_repo)
+		if exists('/etc/yum.repos.d/CentOS-*'):
+			run('mv -f /etc/yum.repos.d/CentOS-* /etc/yum.repos.d/old/')
+	# rpmforge
+	#if not exists('/etc/yum.repos.d/rpmforge.repo'):
+	#	ins_repo = 'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm'
+	#	run('[ ! -f /etc/yum.repos.d/mirrors-rpmforge ] && yum -y -q install '+ ins_repo)
 	run('yum clean all')
 	ins_pkgs = 'system-config-network-tui wget vim git sysstat perl ntp yum-plugin-priorities htop lsof mlocate man openssh-client nc lynx htop bind-utils nfs-utils nfs-utils-lib acpid'
 	run('yum -y -q install '+ ins_pkgs)
@@ -51,6 +53,10 @@ def prep_cent6():
 	for i in stop_service.split():
 		set_service(i, 'stop')
 	run('perl -pi -e "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config')
+
+def test():
+	put('../repo/local-cent6.repo', '/etc/yum.repos.d/')
+
 
 def reboot():
 	run('init 6')
