d3686482cb76831c1c8ec5aaf483480fc7520061
KOO JAHYEON <kjahyeon@gmail.com>
2014-09-25 11:35:13 +0900

    bug fix

--- script/rsync-backup.sh
+++ script/rsync-backup.sh
@@ -1,8 +1,8 @@
 BACKUP_DIR='/newdrive/archive/'
 for i in puppet foreman rpmforge epel6 centos-extra centos-update centos-base
 do
-		BACKUP_FILE="$BACKUPDIR/$i.tar.xz"
-		[ -f "$BACKUP_FILE" ] && rm -f $BACKUP_FILE
-		cd /repo
-		tar -cf - $i | xz -9 -c - > $BACKUP_FILE
+	BACKUP_FILE="$BACKUP_DIR/$i.tar.xz"
+	[ -f "$BACKUP_FILE" ] && rm -f $BACKUP_FILE
+	cd /repo
+	#tar -cf - $i | xz -9 -c - > $BACKUP_FILE
 done
