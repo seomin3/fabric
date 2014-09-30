00aaafa2115a4c0485fed865bb82ae9c3f93206e
KOO JAHYEON <kjahyeon@gmail.com>
2014-10-01 01:13:27 +0900

    fab get.py

---
+++ .vimrc
@@ -0,0 +1,6 @@
+set expandtab
+set softtabstop=4
+set shiftwidth=4
+"set smartindent
+set showmatch
+set history=100
---
+++ fabric.method/get.py
@@ -0,0 +1,27 @@
+from fabric.api import env, task, cd, run, get
+from fabric.colors import magenta, blue
+from fabric.contrib.files import exists
+
+@task
+def hostname():
+    run('hostname')
+
+@task
+def conf():
+    hostname = run('hostname')
+    if exists('/tmp/%s-diff/' % hostname): run('rm -rf /tmp/%s-diff/' % hostname)
+    project = 'keystone glance nova neutron cinder'
+    for i in project.split():
+        if exists('/etc/%s' % i): 
+            run('mkdir -p /tmp/%s-diff/%s' % (hostname, i))
+            with cd('/etc/%s' % i):
+                find = run("find ./ -maxdepth 1 -type f -name '*conf' -o -name '*ini'")
+                for j in find.split(): run("grep -v '^#\|^\s*$' %s > /tmp/%s-diff/%s/%s" % (j, hostname, i, j))
+    with cd('/tmp/%s-diff/' % hostname):
+        run('tar cf - ./ | xz -9 -c - > /tmp/%s-diff.tar.xz' % hostname)
+        get('/tmp/%s-diff.tar.xz' % hostname, '/home/sysop/')
+
+@task
+def etc():
+    with cd('/etc'):
+        run('tar --exclude=./pki --exclude=./selinux/targeted -cf - ./ | xz -9 -c - > /var/tmp/$(hostname)_etc.tar.xz')
