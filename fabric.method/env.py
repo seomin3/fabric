75362beb29d1f1cd6803c9d4c6a5d31e57d33049
KOO JAHYEON <kjahyeon@gmail.com>
2014-10-23 18:24:10 +0900

    cleanup

--- fabric.method/env.py
+++ fabric.method/env.py
@@ -9,6 +9,21 @@ env.command_timeout = 1200
 env.warn_only = False
 env.colorize_errors = True
 
+'''
+try:
+    file = open('hosts.txt', 'r')
+    for data in file:
+        line = data.strip('\n')
+        if line.find('#') != 0: env.hosts.append(line)
+    file.close()
+except IOError as ioerr: print 'no such hosts.txt file: ' + str(ioerr)
+
+nonhost = ''
+for i in nonhost.split():
+    try: env.hosts.remove(i)
+    except ValueError: pass
+'''
+
 #@task
 def get():
     print("Executing on %s as %s" % (magenta(env.host), blue(env.user)))
