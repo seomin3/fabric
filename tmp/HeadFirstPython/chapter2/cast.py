5d46e2a51bd62c7c89dd4376fb6db3f80971b22a
KOO JAHYEON <kjahyeon@gmail.com>
2014-12-05 15:15:12 +0900

    cleanup

--- tmp/HeadFirstPython/chapter2/cast.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/py
-import nester
-
-cast = ['Palin', 'Cleese', 'Idle', 'Jones', 'Gilliam', 'Chapman']
-nester.print_lol(cast)
-
--- tmp/HeadFirstPython/chapter2/headfirst.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/py
-import nester
-
-movies = ['The Holy Grail', 1975, 'Terry Jones & Terry Gilliam', 91,
-	['Graham Chapman', ['Michael Palin', 'John Cleese',
-		'Terry Gilliam', 'Eric Idle', 'Terry Jones']]]
-
-nester.print_lol(movies, 0)
-
